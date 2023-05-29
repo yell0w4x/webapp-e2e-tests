d6f101574b67647f999d8e5ca322aa1e65a5867e
yell0w4x <yellow4x@gmail.com>
2023-05-29 14:04:39 +0300

    Fix google search test failure due changed dom

--- src/test_example.py
+++ src/test_example.py
@@ -11,7 +11,7 @@ from defs import DEFAULT_TIMEOUT
 @pytest.mark.nondestructive
 def test_google_must_search_for_a_query_string(not_logged_in_selenium, search_for):
     driver = not_logged_in_selenium
-    hover_then_click_then_send_keys(driver, '//input[@type="text"]', search_for + Keys.ENTER)
+    hover_then_click_then_send_keys(driver, '(//textarea)[1]', search_for + Keys.ENTER)
     wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
     wait.until(lambda x: driver.title.startswith(search_for))
 
